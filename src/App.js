b877ce509e36fafe2e1b8555543d705a36ef29da
Adam Goth <adamgoth@gmail.com>
2019-07-23 17:47:15 +0200

    Initial set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [link, setLink] = useState("");
+
+  const addRecipe = e => {
+    e.preventDefault();
+    setRecipes([...recipes, { title, link }]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={e => addRecipe(e)}>
+        <h1>add recipe</h1>
+        <label htmlFor="title">title</label>
+        <input
+          onChange={e => setTitle(e.target.value)}
+          name="title"
+          type="text"
+        />
+        <label htmlFor="link">link</label>
+        <input
+          onChange={e => setLink(e.target.value)}
+          name="link"
+          type="text"
+        />
+        <input type="submit" value="add" />
+      </form>
+      <div>
+        {recipes.map(r => (
+          <p>
+            <a key={r.title} href={r.link}>
+              {r.title}
+            </a>
+          </p>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
